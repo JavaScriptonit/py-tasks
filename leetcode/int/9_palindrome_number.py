f06b221008901b553923078d835e5b604f49bfd0
Andrey Shabunov <shabunovaa@yandex.ru>
2024-06-28 14:08:09 +0300

    add 9 task another solution

--- leetcode/int/9_palindrome_number.py
+++ leetcode/int/9_palindrome_number.py
@@ -18,30 +18,48 @@
 # Output: false
 # Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
-import math
 
+# Решение #1:
+
+import math
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # Проверяем, если число x отрицательное
         if (x < 0):
             return False
 
+        # Преобразуем число x в список символов (цифр)
         nums_list = list(f'{x}')
 
-        # length_of_int = len(str(x))
+        # Определяем длину числа (количество цифр)
         length_of_int = len(nums_list)
+
+        # Находим половину длины числа (если число имеет нечетное количество цифр, то центральную цифру не учитываем)
         half_length = math.floor(length_of_int / 2)
-        
         # print(f'dlinna chisla - {half_length}')
-        i = 0
+        
+        i = 0  # Счетчик для перебора цифр в числе
+        # Проходим циклом до середины числа
         for num in range(half_length):
             i += 1
+
             # print(f'nomer iteracii cikla dlinnoi v razmer chisla - {num} & {i}')
+            # Сравниваем цифры с обоих концов числа
             if (nums_list[num] != nums_list[length_of_int - i]):
-                return False
+                return False  # Если цифры не совпадают, число не является палиндромом
                 
-        return True
-        
+        return True   # Если весь цикл выполнен без возврата False, число является палиндромом
+
+
+# Решение #2:
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         a = str(x)  # Преобразуем число x в строку a
+#         if a == a[::-1]:  # Сравниваем строку a с ее реверсом (обратной записью)
+#             return True  # Если строка и ее реверс совпадают, то число является палиндромом
+#         return False  # Если строки не совпадают, число не является палиндромом
 
 
 
